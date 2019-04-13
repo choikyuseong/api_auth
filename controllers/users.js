f66760f41b167618ad10e4853f91df30c4cffdeb
choikyuseong <kusung2230@naver.com>
2019-04-13 10:48:22 +0900

    server-side validation

--- controllers/users.js
+++ controllers/users.js
@@ -2,12 +2,19 @@
 module.exports = {
 
     signUp: async  (req, res, next) => {
-        console.log('UsersController.signUp() called!');
+
+        //Email & password
+        //req,value.body
+        //console.log('UsersController.signUp() called!');
+        console.log('contents or req.value.bod', req.value.body)
+
 
     },
 
     signIn: async  (req, res, next) => {
+        //Generate token
         console.log('UsersController.signIn() called!');
+
 
     },
 
