a1bb4fbe847bf57a6bc3dc4b26cbbf29b9b9ca1f
choikyuseong <kusung2230@naver.com>
2019-04-13 11:59:45 +0900

    Jwt secret test

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = require('express-promise-router')();
+const passport = require('passport');
+const passportConf = require('../passport');
+
+
+
 
 const {validateBody , schemas} = require('../helpers/routehelpers');
 const UsersController = require('../controllers/users');
@@ -14,6 +19,6 @@ router.route('/signin')
     .post(UsersController.signIn);
 
 router.route('/secret')
-    .get(UsersController.secret);
+    .get(passport.authenticate('jwt' , {session:false}),UsersController.secret);
 
 module.exports = router;
