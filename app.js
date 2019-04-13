ea42caa4b7890d41345a55c85891c20796b251cb
choikyuseong <kusung2230@naver.com>
2019-04-13 10:36:59 +0900

    add routes controller users.js

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyparser = require("body-parser");
+const userRouter = require('./routes/users');
+
 
 const app = express();
 
 //Middlewares
 app.use(morgan('dev'));
 app.use(bodyparser.json());
+
+//Routes
+app.use('/users', userRouter);
+
+
 
 //start server
 const port = process.env.port || 3000;
